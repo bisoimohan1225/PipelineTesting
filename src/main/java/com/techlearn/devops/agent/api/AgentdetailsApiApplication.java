e247f1d8996775972d7dd309763aff8986b0313a
Mohan Bisoi <bisoi.mohan@gmail.com>
2021-01-22 22:20:16 -0500

    New microservice project

---
+++ src/main/java/com/techlearn/devops/agent/api/AgentdetailsApiApplication.java
@@ -0,0 +1,13 @@
+package com.techlearn.devops.agent.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentdetailsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentdetailsApiApplication.class, args);
+	}
+
+}
